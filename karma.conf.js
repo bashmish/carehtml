1ea5cb2d8340b9a0e24e1c7a527516de6d3d20fc
Mikhail Bashkirov <bashmish@gmail.com>
2019-08-15 18:48:31 +0200

    chore: autodetect available browsers for testing

--- karma.conf.js
+++ karma.conf.js
@@ -4,11 +4,7 @@ const { createDefaultConfig } = require('@open-wc/testing-karma');
 
 module.exports = (config) => {
   config.set(createDefaultConfig(config));
-  config.browsers = [
-    'ChromeHeadless',
-    'FirefoxHeadless',
-    'Safari',
-  ];
+  config.browsers = [];
   config.files = [
     // can be overwritten by passing a --grep flag. examples:
     //
@@ -16,6 +12,11 @@ module.exports = (config) => {
     // npm run test -- --grep test/bar/*
     { pattern: config.grep ? config.grep : 'test/**/*.spec.js', type: 'module' },
   ];
+  config.frameworks = [...config.frameworks, 'detectBrowsers'];
   config.esm.nodeResolve = true;
+  config.detectBrowsers = {
+    usePhantomJS: false,
+    preferHeadless: true,
+  };
   return config;
 };
