73f26325af1f9b66e46949c7513f055f8bba7ec6
Manolakis <manuel.martin@gmail.com>
2019-12-14 16:24:39 +0100

    feat: add cache
    Adds a cache to improve performance. As a collateral change
    adding a cache we are going to return the same strings instance
    to lit-html, which seems to be a performance requirement.

--- src/wrap.js
+++ src/wrap.js
@@ -1,6 +1,5 @@
 import transform from './transform.js';
 
 export default function wrap(html) {
-  // eslint-disable-next-line prefer-spread
-  return (strings, ...values) => html.apply(null, transform(strings, values));
+  return (strings, ...values) => html(...transform(strings, values));
 }
